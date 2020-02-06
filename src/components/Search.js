8a471594996753abfe46758f7ead7843334bac7b
ajmerr13 <oliverclosoff07@gmail.com>
2020-02-05 19:23:15 -0500

    new

---
+++ src/components/Search.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import SearchForm from "../components/SearchForm";
+
+const Search = props => {
+  const [songResults, setSongResults] = useState([]);
+  const [search, setSearch] = useState({
+    title: ""
+  });
+  console.log("props it", props);
+
+  const handleSearchChange = event => {
+    setSearch({
+      ...search,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  if (!songResults) {
+    return <h2>...loading</h2>;
+  }
+  console.log("songResults", songResults);
+  return (
+    <main>
+      <SearchForm
+        songResults={songResults}
+        handleSearchChange={handleSearchChange}
+        search={search}
+        setSongResults={setSongResults}
+      />
+    </main>
+  );
+};
+export default Search;
+
+{
+  /* <SuggestedCard  
+                            name = {data.name}
+                            url= {data.url}
+                            description= {data.description}
+                            subscribers= {data.subscribers} */
+}
+// />
